4e89a06b9da6e6e3be61aeb533e79521ff355705
Yeonho Jeong <yeonho.dev@gamil.com>
2018-09-07 03:05:06 +0900

    Add test app

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-from google import google
+from datetime import date
 
 app = Flask(__name__)
 
@@ -22,12 +21,41 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
+    headline = "Welcome to Bookhunt! :)"
+    today = date.today().strftime("%b %d, %Y")
+    weekday = date.today().weekday()
+    weekend = weekday == 5 or weekday == 6
+    return render_template("index.html", headline=headline, today=today, weekend=weekend)
 
-@app.route("/<string:name>")
-def hello(name):
-    name = name.capitalize()
-    return f"<h1>Hello, {name}!</h1>"
+@app.route("/names", methods=["GET", "POST"])
+def names():
+    if session.get("names") is None:
+        session["names"] = []
+    if request.method == "POST":
+        name = request.form.get("name")
+        session["names"].append(name)
+
+    return render_template("names.html", names=session["names"])
+
+@app.route("/hello", methods=["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template("hello.html", name=name)
+
+@app.route("/flights", methods=["GET", "POST"])
+def flights():
+    flights = db.execute("SELECT origin, destination, duration FROM flights").fetchall()
+    return render_template("flights.html", flights=flights)
+
+@app.route("/bye")
+def bye():
+    headline = "Goodbye!"
+    return render_template("index.html", headline=headline)
+
+# Comments
+# @app.route("/<string:name>")
+# def hello(name):
+#     name = name.capitalize()
+#     return f"<h1>Hello, {name}!</h1>"
--- google.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def google():
-    res = requests.get("https://www.google.com/")
-    print(res.text)
